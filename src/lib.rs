43c01b619c0167da380a12f3d036e711bec9f1b7
zawa-ch <satellite.2e1834097@gmail.com>
2021-12-23 15:41:31 +0900

    トレイト追加
    HashFunction trait
    ハッシュ関数トレイト

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,19 @@
+/// 任意長のビット列から固定長ビットのダイジェストを生成する関数を表します。
+trait HashFunction {
+    /// ダイジェストに使用される型。
+    type DigestType;
+    /// 空の`HashFunction`を作成し、ハッシュ値の計算を行えるようにします。
+    fn new() -> Self;
+    ///
+    fn put_value(&mut self, value: &u8);
+    ///
+    fn put_message<I: Iterator<Item = u8>>(&mut self, message: I) {
+        for item in message { self.put_value(&item); }
+    }
+    /// 現在のメッセージダイジェストを取得します。
+    fn digest(&self) -> Self::DigestType;
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
